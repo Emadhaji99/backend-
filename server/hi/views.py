154fad0ac548084e01cc6423f8cd0a62befb292c
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-02-12 23:30:04 +0330

    change the response for views

--- server/hi/views.py
+++ server/hi/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request,name):
-    return HttpResponse(f"hello {name}")
-def about(request):
-    return HttpResponse("<h1>say this is about the django</h1>")
-# Create your views here.
+def index(request):
+    return render(request,"hi/index.html") 
