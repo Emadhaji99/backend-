520f9c88efb17c5b6de57110a9b592bf37210982
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-02-12 22:48:34 +0330

    fix the url problem

--- server/hi/urls.py
+++ server/hi/urls.py
@@ -3,6 +3,5 @@ import sys
 sys.path.append("C:\\Users\\Sara Tel\\backend-\\server\\hi")
 import views
 urlpatterns=[
-    path('<str:name>/',views.index,name="hello user"),
-    path("about/",views.about,name="about page"),
+    path('<str:name>',views.index,name="hello user"),
 ]
