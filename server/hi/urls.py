612d13928dfdc617b4284905ef1cff0ab8b5c1a8
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-02-13 17:20:46 +0330

    add logic for greeting request

--- server/hi/urls.py
+++ server/hi/urls.py
@@ -3,5 +3,6 @@ import sys
 sys.path.append("C:\\Users\\Sara Tel\\backend-\\server\\hi")
 import views
 urlpatterns=[
-    path('ad/',views.index,name="greeting")
+    path('ad/',views.index,name="index"),
+    path("<str:tag>",views.greet,name="greet")
 ]
--- server/hi/views.py
+++ server/hi/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return render(request,"hi/index.html") 
+def greet(request,tag):
+    context={
+        "name":tag,
+        "list":["banana","orange"],
+    }
+    return render(request,"hi/greet.html",context)
