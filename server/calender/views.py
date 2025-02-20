09d7b24b4d6f306e9cd3a7089688ea0da0d7d6e7
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-02-20 19:15:49 +0330

    add views logic for project

--- server/calender/views.py
+++ server/calender/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
-date=datetime.now()
 def founder(request):
-    if date.day()==1 and date.month()==1:
-        return render()
-
+    date=datetime.now()
+    print(date)
 # Create your views here.
+
