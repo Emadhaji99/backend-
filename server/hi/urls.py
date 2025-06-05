45b253b8dc67950646078db7d288852329fd9940
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-06-05 15:18:57 +0330

    fixing the hi app

--- server/hi/urls.py
+++ server/hi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('ad/',views.index,name="index"),
+    path('',views.index,name="index"),
     path('<str:tag>/',views.greet,name="greet")
 ]
