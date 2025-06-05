9a596cc0941e0142a5df8fb93c6827aa96d2d051
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-06-05 14:14:29 +0330

    fix the views problem

--- server/hi/urls.py
+++ server/hi/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-import sys
-sys.path.append("C:\\Users\\Sara Tel\\backend-\\server\\hi")
-import views
+from . import views
 urlpatterns=[
     path('ad/',views.index,name="index"),
     path('<str:tag>/',views.greet,name="greet")
