62b1a50df5ccc735fb480327e9350df24904957a
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-06-23 21:33:12 +0330

    reviewing

--- server/todo/views.py
+++ server/todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 def task(request):
-    context={"task":["one","two","three","four"]}
+    context={"task":["one","two","three","four"],}
     return render(request,"todo\\todo.html",context )
 
 def add(request):
