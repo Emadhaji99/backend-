b1c241e7c8e0b31b287c1933168016ff7f0ebdc7
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-06-24 16:55:19 +0330

    reviewing

--- server/todo/urls.py
+++ server/todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path("task/",views.task,name="task"),
-    path("add/",views.add,name="add")
+    path('task/',views.task,name="task"),
+    path('add/',views.add,name="add")
 ]
--- server/todo/views.py
+++ server/todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 def task(request):
     context={"task":["one","two","three","four"],}
-    return render(request,"todo\\todo.html",context )
+    return render(request,"todo\\tasks.html",context )
 
 def add(request):
     return render(request,"todo\\add.html")
