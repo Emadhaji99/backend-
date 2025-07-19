9585d6bbbad130b749cf0d737131e9719d7d103a
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-07-20 00:25:34 +0330

    learning the http request

--- server/todo/views.py
+++ server/todo/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 from .forms import content
 def task(request):
     context={"tas":["one","two","three","four"],}
-    return render(request,"todo\\tasks.html",context )
-
+    return render(request,"todo\\tasks.html",context)
+#-------------------------
 def add(request):
     context={"ta":[1,2,34],"Form":content()}
     return render(request,"todo\\add.html",context)
-
+#-------------------------
 def submit(request):
-    return HttpResponse("the form completed")
+    if request.method=="POST":
+        return HttpResponse("the form completed and the method is post")
+    else:
+        return HttpResponse("this is get method ")
